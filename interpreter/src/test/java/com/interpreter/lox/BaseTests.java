e51efd48acddfc45e65ef6d594d646c5e7b56643
dluman <dluman@allegheny.edu>
2024-01-15 21:55:46 -0500

    Tests work; redirected output is assertable

--- interpreter/src/test/java/com/interpreter/lox/BaseTests.java
+++ interpreter/src/test/java/com/interpreter/lox/BaseTests.java
@@ -1,26 +1,38 @@
 package com.interpreter.lox;
 
-import java.io.*;
-import java.net.*;
-import java.nio.file.*;
-import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
-
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.provider.Arguments;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BaseTests {
 
-  private final static Main lox = new Main();
+  /* 
+    Word to the accepted answer at SO, they're a real one.
+    https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+  */
+
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
+
+  @BeforeAll
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
 
   static Stream<Arguments> testTokenOutput() throws Exception {
     URL resource = BaseTests.class.getClassLoader().getResource("inheritance.lox");
@@ -34,8 +46,17 @@ class BaseTests {
   @MethodSource
   @ParameterizedTest
   void testTokenOutput(String[] args) throws Exception {
-    System.out.println("🖨️\tOUTPUT (mvn exec:java)\r\n");
-    lox.main(args);
+    Main.main(args);
+    assertEquals(
+      "A method",
+      outContent.toString().strip()
+    );
+  }
+
+  @AfterAll
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
   }
 
 }
