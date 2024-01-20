830d091142c7a3f11c957c095f00d259a6454ddc
dluman <dluman@allegheny.edu>
2024-01-20 12:27:07 -0500

    Updating ignore file and tests

--- interpreter/test/java/com/interpreter/lox/BaseTests.java
+++ interpreter/test/java/com/interpreter/lox/BaseTests.java
@@ -1,28 +1,40 @@
 package com.interpreter.lox;
 
-import java.io.*;
-import java.net.*;
-import java.nio.file.*;
-import java.util.Scanner;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
-
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BaseTests {
 
-  private final static Main lox = new Main();
+  /*
+    Word to the accepted answer at SO, they're a real one.
+    https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+  */
 
-  static Stream<Arguments> testTokenOutput() throws Exception {
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
+
+  @BeforeAll
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  static Stream<Arguments> testProgramOutput() throws Exception {
     URL resource = BaseTests.class.getClassLoader().getResource("tokens.lox");
     File file = Paths.get(resource.toURI()).toFile();
     String absPath = file.getAbsolutePath();
@@ -33,18 +45,18 @@ class BaseTests {
 
   @MethodSource
   @ParameterizedTest
-  void testTokenOutput(String[] args) throws Exception {
-    System.out.println("🖨️\tOUTPUT (mvn exec:java)\r\n");
-    lox.main(args);
+  void testProgramOutput(String[] args) throws Exception {
+    Main.main(args);
+    assertEquals(
+      "",
+      outContent.toString().strip()
+    );
   }
 
-  @Test
-  void testErrorMessage() {
-    assertEquals(
-      "[line 0] Error: Test Error",
-      lox.error(0, "Test Error")
-    );
-    lox.hadError = false;
+  @AfterAll
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
   }
 
 }
